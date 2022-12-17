dde98906e75db196ffe31b716fe00b991d0500cf
alixbarreaux <barreaux.alix@kisquadstudio.com>
2022-12-17 16:05:26 +0100

    Refactor: node references

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -4,43 +4,52 @@ signal game_started
 
 ############################### DECLARE VARIABLES ##############################
 
-onready var message_label: Label = $MessagePanel/MessageLabel
+
+# Node References
+onready var message_panel: Panel = $MessagePanel
+onready var message_label: Label = message_panel.get_node("MessageLabel")
+onready var score_label: Label = $ScoreLabel
+onready var message_timer: Timer = $MessageTimer
+onready var play_button: Button = $PlayButton
+onready var credits_button: Button = $CreditsButton
+onready var quit_to_desktop_button: Button = $QuitToDesktopButton
+
 
 ################################# RUN THE CODE #################################
 
-
 ############################### DECLARE FUNCTIONS ##############################
 
+
 func update_score(score: int) -> void:
-	$ScoreLabel.text = str(score)
+	score_label.text = str(score)
 
 
 func show_message(message: String) -> void:
 	message_label.text = message
 	message_label.show()
-	$MessageTimer.start()
+	message_timer.start()
 
 
 func show_game_over() -> void:
 	show_message("Game Over!")
-	yield($MessageTimer, "timeout")
+	yield(message_timer, "timeout")
 	message_label.text = "Dodge the Creeps"
 	message_label.show()
 	yield(get_tree().create_timer(1.0), "timeout")
 	
-	$MessagePanel.show()
-	$Button.show()
-	$CreditsButton.show()
-	$QuitToDesktopButton.show()
+	message_panel.show()
+	play_button.show()
+	credits_button.show()
+	quit_to_desktop_button.show()
 
 
-func _on_Button_pressed() -> void:
-	$Button.hide()
-	$CreditsButton.hide()
-	$QuitToDesktopButton.hide()
+func _on_PlayButton_pressed() -> void:
+	play_button.hide()
+	credits_button.hide()
+	quit_to_desktop_button.hide()
 	self.emit_signal("game_started")
 
 
 func _on_MessageTimer_timeout() -> void:
-	$MessagePanel.hide()
+	message_panel.hide()
 	message_label.hide()
