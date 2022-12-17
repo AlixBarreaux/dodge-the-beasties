d2c904da05b0ad33f9cf8ba672917fce3f7f7da4
alixbarreaux <barreaux.alix@kisquadstudio.com>
2022-12-17 23:30:10 +0100

    Change directories case to kebab-case and reimport translation with .mo files

---
+++ autoloads/settings.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+############################### DECLARE VARIABLES ##############################
+
+
+################################# RUN THE CODE #################################
+
+
+func _ready() -> void:
+	TranslationServer.set_locale(OS.get_locale())
+
+
+############################### DECLARE FUNCTIONS ##############################
